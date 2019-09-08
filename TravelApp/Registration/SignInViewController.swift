52cef744f468d7b1f905bd8ec1a50fdb3107e216
Tigran <tvilasyan@gmail.com>
2019-09-08 11:09:34 +0400

    I have added constrains to ui elemnets

--- TravelApp/Registration/SignInViewController.swift
+++ TravelApp/Registration/SignInViewController.swift
@@ -10,7 +10,22 @@ import UIKit
 
 class SignInViewController: UIViewController {
 
+    @IBAction func emailAddres(_ sender: UITextField) {
+        if emailAddress.isEditing == true{
+            emailAddress.isHighlighted = false
+        }
+    }
+    @IBAction func password(_ sender: UITextField) {
+        if password.isEditing == true{
+            password.isHighlighted = false
+        }
+    }
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var emailAddress: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        emailAddress.borderStyle = .none
+        password.borderStyle = .none
+    
     }
 }
