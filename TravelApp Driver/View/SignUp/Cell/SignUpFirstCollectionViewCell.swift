5d13af479f0480192a71a95b36a3421a2f4c3642
Gerasim <gerasimm1003@gmail.com>
2019-09-23 13:59:40 +0400

    Gerasim/DriverApp created registration steps

---
+++ TravelApp Driver/View/SignUp/Cell/SignUpFirstCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpFirstCollectionViewCell.swift
+//  TravelApp Driver
+//
+//  Created by Gerasim Israyelyan on 9/23/19.
+//  Copyright © 2019 Eric Hovhannisyan. All rights reserved.
+//
+
+import UIKit
+
+class SignUpFirstCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
