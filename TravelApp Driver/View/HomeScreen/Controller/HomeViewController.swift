60dfc873145fabf5ebe74847a12eae10ba85ea8b
Gerasim <gerasimm1003@gmail.com>
2019-09-24 16:32:27 +0400

    Gerasim/DriverApp: created HomeTab

---
+++ TravelApp Driver/View/HomeScreen/Controller/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  TravelApp Driver
+//
+//  Created by Gerasim Israyelyan on 9/24/19.
+//  Copyright © 2019 Eric Hovhannisyan. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
