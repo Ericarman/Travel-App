60d8c30702f28691dbd41917c98e6e5381212472
ericarman <Erichovhannisyan13@gmail.com>
2019-10-05 15:32:22 +0400

    Added OrderTourViewController

---
+++ TravelApp/View/AddTourScreen/Controllers/OrderTourViewController.swift
@@ -0,0 +1,17 @@
+//
+//  OrderTourViewController.swift
+//  TravelApp
+//
+//  Created by Eric Hovhannisyan on 10/5/19.
+//  Copyright © 2019 Eric Hovhannisyan. All rights reserved.
+//
+
+import UIKit
+
+class OrderTourViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
